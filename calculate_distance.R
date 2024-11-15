76d2fdf6dfa25115c8797731390ab3381b795267
Mark B <markkbarrett@gmail.com>
2024-11-15 12:41:53 -0800

    Refactor distance calculation to include Metrolink stops; combine rail and Metrolink data for distance analysis

--- calculate_distance.R
+++ calculate_distance.R
@@ -18,70 +18,41 @@ glimpse(venues)
 (venues_sf = st_as_sf(venues, coords = c("longitude", "latitude"), crs="EPSG:4326", sf_column_name = "venue_geometry")) |> 
   glimpse()
 
-
+# Metro Rail
 # Load rail stops
-la_rail = read_gtfs("https://gitlab.com/LACMTA/gtfs_rail/raw/master/gtfs_rail.zip")
+metro_gtfs = read_gtfs("https://gitlab.com/LACMTA/gtfs_rail/raw/master/gtfs_rail.zip")
 
-(la_rail_sf = gtfs_as_sf(la_rail)) |> 
+(rail_sf = gtfs_as_sf(metro_gtfs)) |> 
   glimpse()
 
-rail_stops = la_rail_sf$stops
-
-rail_stops_only = rail_stops |> 
+rail_stops = rail_sf$stops |> 
   filter(location_type == 0)
 
-# Load bus stops
-la_bus = read_gtfs("https://gitlab.com/LACMTA/gtfs_bus/raw/master/gtfs_bus.zip")
-
-(la_bus_sf = gtfs_as_sf(la_bus)) |> 
-  glimpse()
-
-bus_stops = la_bus_sf$stops
-
+# Metrolink
 # https://metrolinktrains.com/about/gtfs/
+metrolink_gtfs = read_gtfs("https://metrolinktrains.com/globalassets/about/gtfs/gtfs.zip")
+
+metrolink_sf = gtfs_as_sf(metrolink_gtfs)
+
+stops_combined = list_rbind(
+  list(rail_stops, metrolink_sf$stops)
+)
+
+# Metro Bus
+# Load bus stops
+# bus_gtfs = read_gtfs("https://gitlab.com/LACMTA/gtfs_bus/raw/master/gtfs_bus.zip")
+# 
+# (bus_sf = gtfs_as_sf(bus_gtfs)) |> 
+#   glimpse()
+# 
+# bus_stops = bus_sf$stops
+
 
 # Calculate Distance ------------------------------------------------------
 
 # For each row in venues,
 # I want to calculate the distance between the venue and the stop
 # Option #1: Join the stops to the venue and calculate st_distance using two columns
-
-# Find nearest stop
-# 1. Cross join rail stops
-# 2. Calculate distance
-# 3. Keep the stop with the smallest distance
-venues_all_stops = venues_sf |> 
-  # Cross join rail stops
-  cross_join(rail_stops_only) |> 
-  # Calculate distance
-  mutate(distance = as.numeric(st_distance(venue_geometry, geometry, by_element = T)))
-
-# Can use st_join but we don't get nearest stop
-venues_sf |> 
-  st_join(rail_stops_only, st_is_within_distance, dist = units::set_units(1000, "m")) |> View()
-
-# What about st_nearest_points?
-venues_sf |> 
-  st_join(rail_stops_only, st_nearest_feature) |> View()
-
-# Problem: st_join does not return both geometries
-
-# Find nearest stops + nearby (<1km) stops
-nearby_stops = venues_all_stops |> 
-  group_by(venue) |> 
-  filter(distance == min(distance) | distance < 1000) |> View()
-
-
-venues_nearest_stop = venues_all_stops |> 
-  # Group by venue and keep min distance
-  group_by(venue) |> 
-  filter(distance == min(distance, na.rm=T)) |> 
-  ungroup()
-
-# Find stops within 1000m
-venues_sf |> 
-  st_join(rail_stops_only, st_is_within_distance, dist = units::set_units(1000, "m"))
-
 
 
 #' Add Nearby Stops
@@ -97,59 +68,102 @@ venues_sf |>
 #' @export
 #'
 #' @examples
-add_nearby_stops = function(df, rail_stops_df, within_distance = 1000){
+calculate_stop_distances = function(venue, venue_geometry, rail_stops_df, within_distance = 1000){
   # stops_within_dist = st_join(rail_stops_df, df, st_is_within_distance, left=F, dist = units::set_units(within_distance, "m"))
   # stops_list = list(stops_within_dist)
-  
-  venue_geometry_only = select(df, venue, venue_geometry)
+  # browser()
+  # venue_geometry_only = select(df, venue, venue_geometry)
+  venue_geometry_only = st_sf(
+    venue = venue, 
+    venue_geometry = st_sfc(venue_geometry), 
+    crs="EPSG:4326"
+  )
   
   # Calculate distances between venue and stops
   stop_distances = cross_join(venue_geometry_only, rail_stops_df) |> 
     mutate(distance = as.numeric(st_distance(venue_geometry, geometry, by_element=T))) |> 
     # Keep stops within distance or the minimum
-    filter(distance <= !!within_distance | distance == min(distance)) |> 
+    # filter(distance <= !!within_distance | distance == min(distance)) |> 
     select(-starts_with("venue")) |> 
     # For reasons I can't understand, this only removes venue geometry
     st_set_geometry(NULL)
   
+  stop_distances
   # Filter for the nearest stop
-  nearest_stop = stop_distances |> 
-    filter(distance == min(distance))
-  
-  # Add both sets of stops as list columns
-  mutate(df, 
-         nearest_stop = list(nearest_stop),
-         stops_within_1km = list(stop_distances))
+  # nearest_stop = stop_distances |> 
+  #   filter(distance == min(distance))
+  # 
+  # # Add both sets of stops as list columns
+  # mutate(df, 
+  #        nearest_stop = list(nearest_stop),
+  #        stops_within_1km = list(stop_distances))
 }
 
 # For each nearby stop, what routes are available?
 add_routes = function(df, gtfs_data){
   routes = distinct(gtfs_data$stop_times, stop_id, route_code)
+  # browser()
+  # stops_within = unnest(df, stops_within_1km)
   
-  stops_within = unnest(df, stops_within_1km)
-  
-  routes_within = stops_within |> 
+  routes_within = df |> 
     left_join(routes, by = "stop_id") |> 
     inner_join(gtfs_data$routes, by = c("route_code" = "route_long_name")) |> 
     distinct(route_code, route_id, route_type, route_color, route_text_color, route_url)
   
-  mutate(df, routes = list(routes_within))
+  # mutate(df, routes = list(routes_within))
+  routes_within
 }
 
 # Split venues into list of rows
 venues_split = venues_sf |> 
   split(venues_sf$venue)
 
+
+
+venues_metro = venues_sf |> 
+  mutate(metro_stop_distances = map2(venue, venue_geometry, \(venue, venue_geometry) calculate_stop_distances(venue, venue_geometry, stops_combined)),
+         metro_nearest_stop = map(metro_stop_distances, \(stops) filter(stops, distance == min(distance))),
+         metro_nearby_stops = map(metro_stop_distances, \(stops) filter(stops, distance < 1000)),
+         metro_nearby_routes = map(metro_nearby_stops, \(stops) add_routes(stops, metro_gtfs)))
+
+
 venues_complete = venues_split |> 
-  map(\(venue) as_tibble(add_nearby_stops(venue, rail_stops_only))) |> 
-  map(\(venue) as_tibble(add_routes(venue, la_rail))) |> 
-  list_rbind()
+  # map(\(venue) as_tibble(add_nearby_stops(venue, rail_stops_only))) |> 
+  
+  # map(\(venue) as_tibble(add_routes(venue, metro_gtfs))) |> 
+  list_rbind() |> 
+  mutate(metro_routes = map(stops_within_1km, \(stops) add_routes(stops, metro_gtfs)))
 
 
 
 # Write -----------------------------------------------------------
 
 # To RDS
-venues_complete |> write_rds(here("data", "venues_complete.rds"))
+write_rds(venues_complete, here("data", "venues_complete.rds"))
+
 # To JSON
-jsonlite::toJSON(venues_complete) |> write_lines(here("data", "venues_complete.json"))
+jsonlite::toJSON(venues_complete) |> 
+  write_lines(here("data", "venues_complete.json"))
+
+
+# Rough Work --------------------------------------------------------------
+
+# Find nearest stop
+# 1. Cross join rail stops
+# 2. Calculate distance
+# 3. Keep the stop with the smallest distance
+# venues_all_stops = venues_sf |> 
+#   # Cross join rail stops
+#   cross_join(rail_stops_only) |> 
+#   # Calculate distance
+#   mutate(distance = as.numeric(st_distance(venue_geometry, geometry, by_element = T)))
+# 
+# # Can use st_join but we don't get nearest stop
+# venues_sf |> 
+#   st_join(rail_stops_only, st_is_within_distance, dist = units::set_units(1000, "m"))
+# 
+# # What about st_nearest_points?
+# venues_sf |> 
+#   st_join(rail_stops_only, st_nearest_feature)
+
+# Problem: st_join does not return both geometries
