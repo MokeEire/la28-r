8565795078a5908c2436bfe0d7018d719380413c
Mark B <markkbarrett@gmail.com>
2025-05-23 11:12:12 -0700

    Add time of day, driving, and update output locations for isochrones

--- R/prepare-isochrones.R
+++ R/prepare-isochrones.R
@@ -5,29 +5,19 @@ library(tidyverse)
 library(sf)
 library(traveltimeR)
 library(jsonlite)
+library(tictoc)
 source(here("R", "fct-traveltime.R"))
 
 # Read in data ------------------------------------------------------------
+tic("Prepare Venues Data")
+source(here("R", "prepare-venues-data.R"))
+toc()
 
 ## Venues -----------------------------------------------------------------
 
 venues = read_rds(here("data", "venues_complete.rds"))
 
 glimpse(venues)
-
-# Some of the venues are at the same address e.g. different stadia in Carson.
-# I can reduce the API calls I need to make by only using the distinct addresses.
-
-# Distinct addresses for venues
-venues_distinct_addresses = venues |> 
-  # combine events for the venue
-  group_by(venue_simplified) |> 
-  mutate(events = str_c(events, collapse = ", "),
-         venues = str_c(venue, collapse = ", ")) |> 
-  ungroup() |> 
-  distinct(venue_simplified, address, cluster, events, venue_geometry, .keep_all = T)
-
-glimpse(venues_distinct_addresses)
 
 ## Population data (NHGIS) -----------------------------------------------
 
@@ -78,81 +68,158 @@ glimpse(nhgis_la_shp_subset)
 # Create API Requests -----------------------------------------------------
 
 # Calculate lat/lon and put them in a list column to match format for API calls
-venues_iso = venues_distinct_addresses |> 
+venues_iso = venues |> 
   mutate(lat = st_coordinates(venue_geometry)[,2], 
          lng = st_coordinates(venue_geometry)[,1],
          coords = map2(lat, lng, \(lat, lng) list(lat = lat, lng = lng))) |>
-  select(id = venue_simplified, coords) |> 
+  select(id = venue_location, coords) |> 
   st_drop_geometry()
 
 # Create a combination of venues and travel times for isochrone requests
 travel_times = 60*c(30, 60, 90, 120)
 
 iso_request_df = venues_iso |> 
-  expand_grid(travel_time = travel_times)
+  expand_grid(travel_time = 60*c(30, 60, 90, 120),
+              time_of_day = c(9, 11, 13))
 
 glimpse(iso_request_df)
 
+
+## Transit -----------------------------------------------------------------
+
 # Create a search object for each venue: public transport
 venues_iso_search = iso_request_df |> 
-  pmap(\(id, coords, travel_time) make_search(id = str_c(id, "_", travel_time), 
+  pmap(\(id, coords, travel_time, time_of_day) make_search(id = str_c(id, "_", travel_time), 
                                               travel_time = travel_time, 
-                                              arrival_time = "2024-12-18T09:00:00-08:00", 
+                                              arrival_time = next_weekday(time_in_hrs = time_of_day),#"2024-12-18T09:00:00-08:00", 
                                               coords = coords, 
                                               transportation = list(type = "public_transport", 
-                                                                    walking_time=20*60, 
+                                                                    walking_time=15*60, 
                                                                     pt_change_delay = 120),
                                               level_of_detail = list(scale_type = "simple", level = "medium")))
 
+tic("Make API Requests: Transit")
 # Perform searches
 isochrones_transit_results = map(venues_iso_search, time_map_sf)
+toc()
 
+# Combine into a single data frame
 isochrones_transit_df = list_rbind(isochrones_transit_results, names_to = "id")
 
+
+## Driving -----------------------------------------------------------------
+
+venues_iso_search_drive = iso_request_df |> 
+  pmap(\(id, coords, travel_time, time_of_day) make_search(id = str_c(id, "_", travel_time), 
+                                                           travel_time = travel_time, 
+                                                           arrival_time = next_weekday(time_in_hrs = time_of_day), 
+                                                           coords = coords, 
+                                                           transportation = list(type = "driving"),
+                                                           level_of_detail = list(scale_type = "simple", level = "medium")))
+
+tic("Make API Requests: Driving")
+# Perform searches
+isochrones_drive_results = map(venues_iso_search_drive, time_map_sf)
+toc()
+# Combine into a single data frame
+isochrones_drive_df = list_rbind(isochrones_drive_results, names_to = "id")
+
 ## Alternatively: Load isochrone from files -------------------------------
-isochrones_transit_df = readRDS(here("data", str_c("isochrones_transit", "2024-12-16", ".Rds")))
-isochrones_drive_df = readRDS(here("data", str_c("isochrones_drive", "2024-12-16", ".Rds")))
+# isochrones_transit_df = readRDS(here("data", str_c("isochrones_transit", "2024-12-16", ".Rds")))
+# isochrones_drive_df = readRDS(here("data", str_c("isochrones_drive", "2024-12-16", ".Rds")))
 
-
+tic("Combine Isochrones + Venues")
 # Combine Isochrones + Venues ---------------------------------------------
 
 isochrones_transit_venues = isochrones_transit_df |> 
   # Join with venues to get the venue data
-  inner_join(venues_distinct_addresses, by = "venue") |> 
+  inner_join(venues, by = c("venue" = "venue_location")) |> 
   # Create travel time factor var
   mutate(travel_time_mins = factor(travel_time/60))|> 
   arrange(desc(travel_time))
 
+isochrones_drive_venues = isochrones_drive_df |> 
+  # Join with venues to get the venue data
+  inner_join(venues, by = c("venue" = "venue_location")) |> 
+  # Create travel time factor var
+  mutate(travel_time_mins = factor(travel_time/60))|> 
+  arrange(desc(travel_time))
+
+toc()
 
 # Join Population Data ----------------------------------------------------
 
+tic("Join Population Data")
+## Transit -----------------------------------------------------------------
+
 # Combine multiple polygons per Venue/Time into one MultiPolygon
-isochrones_union = isochrones_transit_venues |> 
+isochrones_transit_combined = isochrones_transit_venues |> 
   # Convert multiple individual polygons to single multipolygon
-  group_by(id, venue_simplified, venues, travel_time, travel_time_mins, 
+  group_by(id, venue, venues, travel_time, travel_time_mins, 
+           arrival_time, transportation) |>
+  summarise(geometry = st_union(geometry), .groups = "drop") |>
+  st_as_sf()
+
+
+# Spatial join isochrones to census tracts where they intersect
+isochrones_transit_pop = isochrones_transit_combined |> 
+  st_join(y=nhgis_la_shp_subset, join = st_intersects)
+
+# Calculate the % of total population within each isochrone
+total_population = sum(nhgis_la_shp_subset$POPULATION)
+
+isochrones_transit_pop_pct = isochrones_transit_pop |> 
+  st_drop_geometry() |> 
+  group_by(venue, venues, travel_time, travel_time_mins, arrival_time, transportation) |>
+  summarise(pop = sum(POPULATION),
+            pop_pct = pop/total_population, .groups = "drop")
+
+# Join pop % data with isochrones
+isochrones_transit_output = left_join(isochrones_transit_combined,
+                                      isochrones_transit_pop_pct,
+                                 by = c("venue", "venues", "travel_time", "travel_time_mins", "transportation", "arrival_time"))
+
+## Driving -----------------------------------------------------------------
+
+# Combine multiple polygons per Venue/Time into one MultiPolygon
+isochrones_drive_combined = isochrones_drive_venues |> 
+  # Convert multiple individual polygons to single multipolygon
+  group_by(id, venue, venues, travel_time, travel_time_mins, 
            arrival_time, transportation, walking_time, pt_change_delay) |>
   summarise(geometry = st_union(geometry), .groups = "drop") |>
   st_as_sf()
 
 
 # Spatial join isochrones to census tracts where they intersect
-isochrones_pop = isochrones_union |> 
+isochrones_drive_pop = isochrones_drive_combined |> 
   st_join(y=nhgis_la_shp_subset, join = st_intersects)
 
-# Calculate the % of total population within each isochrone
-total_population = sum(nhgis_la_shp_subset$POPULATION)
 
-isochrones_pop_pct = isochrones_pop |> 
+isochrones_drive_pop_pct = isochrones_drive_pop |> 
   st_drop_geometry() |> 
-  group_by(venue_simplified, travel_time, travel_time_mins, arrival_time, transportation, walking_time, pt_change_delay) |>
+  group_by(venue, venues, travel_time, travel_time_mins, arrival_time, transportation, walking_time, pt_change_delay) |>
   summarise(pop = sum(POPULATION),
             pop_pct = pop/total_population, .groups = "drop")
 
 # Join pop % data with isochrones
-isochrones_output_df = left_join(isochrones_union,
-                                 isochrones_pop_pct,
-                                 by = c("venue_simplified", "travel_time", "travel_time_mins", "transportation", "walking_time", "pt_change_delay", "arrival_time"))
+isochrones_drive_output = left_join(isochrones_drive_combined,
+                                      isochrones_drive_pop_pct,
+                                      by = c("venue", "venues", "travel_time", "travel_time_mins", "transportation", "walking_time", "pt_change_delay", "arrival_time"))
+
+toc()
 
 # Write Data --------------------------------------------------------------
 
-st_write(isochrones_output_df, here("output", "isochronesTransit2025.geojson.json"), driver = "GeoJSON", append = F)
+tic("Writing Data")
+st_write(isochrones_transit_output, 
+         here("output", str_c("isochronesTransit", today(), ".geojson.json")), 
+         driver = "GeoJSON", append = F)
+
+st_write(isochrones_drive_output, 
+         here("output", str_c("isochronesDrive", today(), ".geojson.json")), 
+         driver = "GeoJSON", append = F)
+
+isochrones_df = list_rbind(list(isochrones_transit_output, isochrones_drive_output))
+saveRDS(isochrones_df, here("output", str_c("isochrones", today(), ".Rds")))
+
+toc()
