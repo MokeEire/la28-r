a80cb6141a5b087c73f33820d8a377754f8cbd7f
Mark B <markkbarrett@gmail.com>
2025-10-16 11:09:39 -0700

    Move plot to quarto doc

--- R/collect-traveltimes.R
+++ R/collect-traveltimes.R
@@ -92,44 +92,4 @@ travel_times |>
 saveRDS(travel_times, here("data", "traveltime", str_c("travel_times_", today(),".rds")))
 saveRDS(travel_times, here("data", "traveltime", str_c("travel_times_current.rds")))
 
-# Plot
 
-travel_times |> 
-  select(id, venue, arrival_time, transportation, time_mins) |> 
-  pivot_wider(id_cols = c(id, venue, arrival_time), names_from = transportation, values_from = c(time_mins), id_expand = T) |> 
-  mutate(driving = driving*1.075, 
-         public_transport = public_transport*.925) |>
-  arrange(venue, id) |> 
-  mutate(time_diff = public_transport - driving,
-         time_diff_ratio = public_transport/driving) |> 
-  group_by(venue) |> 
-  mutate(mean_time_ratio = mean(time_diff_ratio, na.rm=T),
-         mean_midpoint = mean(driving, na.rm=T) + mean(time_diff, na.rm=T)/2) |> 
-  ungroup() |> 
-  arrange(desc(mean_time_ratio)) |> 
-  mutate(venue = fct_inorder(venue)) |> 
-  pivot_longer(cols = c(driving, public_transport), names_to = "transport_mode", values_to = "time_mins") |> 
-  group_by(venue, transport_mode)|> 
-  mutate(mean_time = mean(time_mins, na.rm=T)) |> 
-  ungroup() |> 
-  ggplot(aes(x = time_mins)) +
-  # Geoms: vertical mean lines, histograms, average difference ratio, 
-  geom_vline(aes(xintercept = mean_time, colour = transport_mode),
-             linetype = "dashed", linewidth = 1, alpha = 0.95, show.legend = F) +
-  geom_histogram(aes(fill = transport_mode), position = "identity", alpha = 0.75, bins = 30, colour = "white") +
-  geom_text(aes(x = mean_time, colour = transport_mode, label = scales::comma(mean_time, suffix = " mins", accuracy = 1), 
-                hjust = if_else(transport_mode == "driving", 1, 0),
-                nudge_x = if_else(transport_mode == "driving", -5, 5)), y = 550, size = 3,
-             show.legend = F) +
-  geom_text(aes(x = mean_midpoint, label = scales::comma(mean_time_ratio, suffix = "x", accuracy = .1)), y = 620,
-            show.legend = F)+
-  facet_wrap(~venue) +
-  # Scales
-  scale_fill_manual(values = c("driving" = "#5993D1", "public_transport" = "#f5a623"), breaks = c("Driving" = "driving", "Transit" = "public_transport")) +
-  scale_colour_manual(values = c("driving" = "#5186BE", "public_transport" = "#EA940B")) +
-  labs(title = "Travel Time Distribution by Transport Mode",
-       x = "Travel Time (minutes)",
-       y = "Count",
-       fill = "Transport Mode") +
-  theme_minimal(base_size = 16)+
-  theme(legend.position = "top")
